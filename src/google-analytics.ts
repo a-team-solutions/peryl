fcff2ca566aea09998127547f6a5b2cc6854f524
Peter Rybar <pr.rybar@gmail.com>
2019-02-21 12:34:48 +0100

    analytics - update to gtag

--- src/google-analytics.ts
+++ src/google-analytics.ts
@@ -8,66 +8,35 @@ export class GA {
     constructor(trackingId: string, dimensions?: Object) {
         this._trackingId = trackingId;
         this._load();
+    }
+
+    pageview(path: string): void {
         if (this._trackingId) {
-            const w: any = self;
-            w.ga("create", trackingId, "auto");
-            if (dimensions) {
-                for (const prop in dimensions) {
-                    if (dimensions.hasOwnProperty(prop)) {
-                        // console.log(prop + ": " + dimensions[prop]);
-                        w.ga("set", prop, (dimensions as any)[prop]);
-                    }
-                }
-            }
-            w.ga("send", "pageview");
+            (self as any).gtag("config", this._trackingId, { page_path: path });
         }
     }
 
-    sendPageview(path: string): void {
+    event(eventName: string, eventParameters: { [k: string]: string }): void {
         if (this._trackingId) {
-            const w: any = self;
-            w.ga("send", "pageview", path);
-
-            // w.ga("set", { page: path, title: title});
-            // w.ga("send", "pageview");
-
-            // w.ga("set", "page", path);
-            // w.ga("send", "pageview");
-        }
-    }
-
-    sendEvent(category: string, action: string, label: string): void {
-        if (this._trackingId) {
-            const ga = (self as any).ga;
-            // console.log("Senging GA", ga, category, action, label);
-            if (ga) {
-                ga("send", "event", category, action, label);
-                // ga("send", "pageview", {
-                //     "page": this.get("url"),
-                //     "title": this.get("url")
-                // });
-            }
+            (self as any).gtag("event", eventName, eventParameters);
         }
     }
 
     private _load(): void {
         if (this._trackingId) {
-            (function (i: any, s: any, o: any, g: any, r: any, a?: any, m?: any) {
-                i["GoogleAnalyticsObject"] = r;
-                i[r] = i[r] || function () {
-                        ((<any>i[r]).q = (<any>i[r]).q || []).push(arguments);
-                    };
-                (<any>i[r]).l = 1 * (<any>new Date());
-                a = s.createElement(o);
-                m = s.getElementsByTagName(o)[0];
-                a.async = 1;
-                a.src = g;
-                if (m !== undefined) {
-                    m.parentNode.insertBefore(a, m);
-                } else {
-                    document.head.appendChild(a);
-                }
-            })(self, document, "script", "//www.google-analytics.com/analytics.js", "ga");
+            const script = <HTMLScriptElement> document.createElement("script");
+            script.async = true;
+            script.src = `https://www.googletagmanager.com/gtag/js?id=${this._trackingId}`;
+            document.getElementsByTagName("head")[0].appendChild(script);
+
+            const script1 = <HTMLScriptElement> document.createElement("script");
+            script1.textContent = `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${this._trackingId}');
+            `;
+            document.getElementsByTagName("head")[0].appendChild(script1);
         }
     }
 
@@ -75,4 +44,4 @@ export class GA {
 
 // const ga = new GA("track-id");
 // GA.ga = ga;
-// ga.sendPageview("my-page");
+// ga.pageview("my-page");
