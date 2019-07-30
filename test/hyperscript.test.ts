36bdfa45a801103ad424a4124a2b1450e335276a
Peter Rybar <pr.rybar@gmail.com>
2019-07-30 07:36:23 +0200

    hyperscript test fix

--- test/hyperscript.test.ts
+++ test/hyperscript.test.ts
@@ -3,34 +3,39 @@ import { div, a, ul, li, input, h2, button } from "../src/hyperscript";
 
 const todo = (text: string) => li([
     a({ href: "#" }, [text])
-
 ]);
 
 describe("hypescript", () => {
 
     it("should be equal with hsml", () => {
-        expect(div("#todo-app", [
-            h2(["Todo App"]),
-            div(".main", [
-                input(".input-text", { type: "checkbox" }, ["placeholder"]),
-                button({ onclick: ["click"] }),
-                ul(".todos", ["first", "second", "third"].map(txt => todo(txt)))
-            ])
-        ])).toEqual(["div#todo-app", [
-            ["h2", "Todo App"],
-            ["div.main", [
-                ["input.input-text", { type: "checkbox" }, "placeholder"],
-                ["button", { onclick: ["click"] }],
-                ["ul.todos", [
-                    ["li",
-                        [["a", { href: "#" }, "first"]]],
-                    ["li",
-                        [["a", { href: "#" }, "second"]]],
-                    ["li",
-                        [["a", { href: "#" }, "third"]]]
-                ]]
-            ]]]] as any);
+        expect(
+            div("#todo-app", [
+                h2(["Todo App"]),
+                div(".main", [
+                    input(".input-text", { type: "checkbox" }, ["placeholder"]),
+                    button({ onclick: ["click"] }),
+                    ul(".todos", ["first", "second", "third"].map(txt => todo(txt)))
+                ])
+            ]))
+            .toEqual(
+                ["div#todo-app", [
+                    ["h2", ["Todo App"]],
+                    ["div.main", [
+                        ["input.input-text", { type: "checkbox" }, ["placeholder"]],
+                        ["button", { onclick: ["click"] }],
+                        ["ul.todos", [
+                            ["li", [
+                                ["a", { href: "#" }, ["first"]]
+                            ]],
+                            ["li", [
+                                ["a", { href: "#" }, ["second"]]
+                            ]],
+                            ["li", [
+                                ["a", { href: "#" }, ["third"]]
+                            ]]
+                        ]]
+                    ]]
+                ]]);
     });
 
 });
-
