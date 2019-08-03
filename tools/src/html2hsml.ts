bd5d89e45ddffea066da08fb4e6455e7b5d5a85c
Peter Rybar <pr.rybar@gmail.com>
2019-08-03 19:16:30 +0200

    tools - update deps, jsonml removed

--- tools/src/html2hsml.ts
+++ tools/src/html2hsml.ts
@@ -75,10 +75,10 @@ export function html2hsml(html: string): any {
         let children = currentNode[currentNode.length - 1];
         if (children.length === 0) {
             currentNode.pop(); // remove children
-        } else if (children.length === 1) {
-            if (typeof children[0] === "string") {
-                currentNode[currentNode.length - 1] = children[0];
-            }
+        // } else if (children.length === 1) {
+        //     if (typeof children[0] === "string") {
+        //         currentNode[currentNode.length - 1] = children[0];
+        //     }
         }
         pointer = nodePath[nodePath.length - 1];
         // console.log(">>>", hsmlPath.map(x => x[0]));
@@ -96,54 +96,61 @@ export function html2hsml(html: string): any {
 }
 
 export function hsml2string(hsml: any): string {
-    return JSON.stringify(hsml, null, 4).replace(/\[\s+"/mg, `["`);
+    // return JSON.stringify(hsml, null, 4).replace(/\[\s+"/mg, `["`);
+    return hsml2strArray(hsml).join("\n");
 }
 
-// const html = '<body>Hello<hr id="id1" class="c1 c2" data-x="dx"  data-y="dy" />, <em name="world">world</em>!</body>';
+function hsml2strArray(hsml: any, out: string[] = [], depth = 0): string[] {
+    switch (hsml.constructor) {
+        case Array:
+            out.push(mkIndent(depth) + "[" + JSON.stringify(hsml[0]) +
+                (hsml.length > 1 ? "," : ""));
+            for (let i = 1; i < hsml.length; i++) {
+                hsml2strArray(hsml[i], out, depth + 1);
+            }
+            out.push(mkIndent(depth) + "]");
+            break;
+        // case Function:
+        //     break;
+        case String:
+            out.push(mkIndent(depth) + JSON.stringify(hsml) +
+                (hsml.length > 1 ? "," : ""));
+            break;
+        default: // Object
+            out.push(mkIndent(depth) + JSON.stringify(hsml) +
+                (hsml.length > 2 ? "," : ""));
+    }
+    return out;
+}
+
+function mkIndent(count: number) {
+    let indent = "";
+    for (let i = 0; i < count; i++) {
+        indent += "    ";
+    }
+    return indent;
+}
+
+// Test -----------------------------------------------------------------------
+
+// const html = `
+//     <body>
+//         Hello
+//         <p>
+//             text <b>text</b>
+//         </p>
+//         <hr id="id1" class="c1 c2" data-x="dx" data-y="dy" />,
+//         <em name="world">world</em>!
+//     </body>`;
 // console.log(html, "\n");
 
 // const hsml = html2hsml(html);
 
-// // console.log(hsml);
+// console.log(hsml);
 // // console.log(JSON.stringify(hsml));
 // console.log(JSON.stringify(hsml, null, 4));
 
-
-
-
-// function mkIndent(count) {
-//     let indent = "";
-//     for (let i = 0; i < count; i++) {
-//         indent += "    ";
-//     }
-//     return indent;
-// }
-
-// function hsmlPrint(node, depth=0) {
-//     switch (node.constructor) {
-//         case Array:
-//             console.log(mkIndent(depth) + "[" + JSON.stringify(node[0]) +
-//                 (node.length > 1 ? "," : ""));
-//             for (const i = 1; i < node.length; i++) {
-//                 hsmlPrint(node[i], depth + 1);
-//             }
-//             console.log(mkIndent(depth) + "]");
-//             break;
-//         // case Function:
-//         //     break;
-//         case String:
-//             console.log(mkIndent(depth) + JSON.stringify(node) +
-//                 (node.length > 1 ? "," : ""));
-//             break;
-//         default: // Object
-//             console.log(mkIndent(depth) + JSON.stringify(node) +
-//                 (node.length > 2 ? "," : ""));
-//     }
-// }
-
-// const html = '<body>Hello<hr id="id1" class="c1 c2" data-x="dx"  data-y="dy" />, <em name="world">world</em>!</body>';
-// console.log(html, "\n");
-
 // const hsml = html2hsml(html);
 
-// hsmlPrint(hsml);
+// const hsmlStr = hsml2string(hsml);
+// console.log(hsmlStr);
