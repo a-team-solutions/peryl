c864dfec30b58267a006f5d7c3928d23fd80095a
Peter Rybar <pr.rybar@gmail.com>
2019-09-07 17:42:08 +0200

    processor demo update

--- demo/processor_demo.ts
+++ demo/processor_demo.ts
@@ -1,17 +1,19 @@
 
-interface BaseState {
+interface State {
     id: string;
     processor: string;
     processors: string[];
 }
 
-interface State extends BaseState {
-    data: string;
+interface TicketState extends State {
+    user: string;
+    gass: number;
+    ticket: string;
 }
 
-const states = [] as State[];
+const states: TicketState[] = [];
 
-abstract class Procesor<S extends BaseState> {
+abstract class Procesor<S extends State> {
 
     readonly name: string;
     readonly states: S[] = [];
@@ -31,7 +33,6 @@ abstract class Procesor<S extends BaseState> {
         this.name && (state.processor = this.name);
         this.name && state.processors.push(this.name);
         this.process(state);
-        // state.processor = "";
     }
 
     run() {
@@ -46,17 +47,17 @@ abstract class Procesor<S extends BaseState> {
 
 }
 
-class Producer extends Procesor<State> {
+class OrderTicket extends Procesor<TicketState> {
 
     private counter: number = 0;
     private count: number;
 
-    constructor(states: State[], interval: number, count: number) {
-        super("p", states, interval);
+    constructor(states: TicketState[], interval: number, count: number) {
+        super("OrderTicket", states, interval);
         this.count = count;
     }
 
-    select(states: State[]): State[] {
+    select(states: TicketState[]): TicketState[] {
         if (this.counter < this.count) {
             console.log(JSON.stringify(this.name));
             this.counter++;
@@ -64,84 +65,119 @@ class Producer extends Procesor<State> {
                 id: "" + this.counter,
                 processor: "",
                 processors: [this.name],
-                data: ""
+                user: `user-${this.counter}`,
+                gass: 0,
+                ticket: ""
             });
         }
         return [];
     }
 
-    process(state: State): void {
+    process(state: TicketState): void {
     }
 
 }
 
-class DataX extends Procesor<State> {
+class LoadGass extends Procesor<TicketState> {
 
-    constructor(states: State[], interval: number) {
-        super("x", states, interval);
+    constructor(states: TicketState[], interval: number) {
+        super("LoadGass", states, interval);
     }
 
-    select(states: State[]): State[] {
+    select(states: TicketState[]): TicketState[] {
         console.log(JSON.stringify(this.name));
-        return states.filter(s => !s.data);
+        return states.filter(s => !s.gass);
     }
 
-    process(state: State): void {
-        state.data = "x";
+    process(state: TicketState): void {
+        const gass = this.tryLoadGas(state.user);
+        if (gass) {
+            state.gass = gass;
+        }
+    }
+
+    tryLoadGas(userId: string): number {
+        const gass = random(3);
+        return gass;
     }
 
 }
 
-class DataY extends Procesor<State> {
+class BuyTicket extends Procesor<TicketState> {
 
-    constructor(states: State[], interval: number) {
-        super("y", states, interval);
+    constructor(states: TicketState[], interval: number) {
+        super("BuyTicket", states, interval);
     }
 
-    select(states: State[]): State[] {
+    select(states: TicketState[]): TicketState[] {
+        return states.filter(s => s.gass);
+    }
+
+    process(state: TicketState): void {
+        const ticket = this.buyTicket(state.user);
+        if (ticket) {
+            state.ticket = ticket;
+        }
+    }
+
+    buyTicket(user: string): string {
+        if (random(3)) {
+            return `${user}-ticket`;
+        } else {
+            return "";
+        }
+    }
+
+}
+
+class ConfirmTicket extends Procesor<TicketState> {
+
+    constructor(states: TicketState[], interval: number) {
+        super("ConfirmTicket", states, interval);
+    }
+
+    select(states: TicketState[]): TicketState[] {
         console.log(JSON.stringify(this.name));
-        return states.filter(s => s.data === "x");
+        return states.filter(s => s.ticket);
     }
 
-    process(state: State): void {
-        state.data = "y";
+    process(state: TicketState): void {
+        if (this.confirmTicket(state.user, state.ticket)) {
+            this.states.splice(this.states.indexOf(state), 1);
+        }
     }
 
-}
-
-class Deleter extends Procesor<State> {
-
-    constructor(states: State[], interval: number) {
-        super("d", states, interval);
-    }
-
-    select(states: State[]): State[] {
-        console.log(JSON.stringify(this.name));
-        return states.filter(s => s.data === "y");
-    }
-
-    process(state: State): void {
-        this.states.splice(this.states.indexOf(state), 1);
+    confirmTicket(user: string, ticket: string): boolean {
+        if (!!random(5)) {
+            console.log("mail confirmation:", user, ticket);
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
 
 
-class Viewer extends Procesor<State> {
+class ViewTicketStates extends Procesor<TicketState> {
 
-    constructor(states: State[], interval: number) {
+    constructor(states: TicketState[], interval: number) {
         super("", states, interval);
     }
 
-    select(states: State[]): State[] {
+    select(states: TicketState[]): TicketState[] {
         console.log(JSON.stringify(this.name));
         log(states.map(s => JSON.stringify(s)).join("\n"));
         return states;
     }
 
-    process(state: State): void {
+    process(state: TicketState): void {
     }
 
+}
+
+function random(max: number) {
+    return Math.floor(Math.random() * Math.floor(max));
 }
 
 
@@ -152,12 +188,12 @@ function log(...msgs: any[]) {
     out!.textContent = msgs.join(" ");
 }
 
-document.getElementById("start")!.addEventListener("click", e => start());
+document.getElementById("start")!.addEventListener("click", () => run());
 
-function start() {
-    new Producer(states, 300, 10e3).run();
-    new DataX(states, 3e3).run();
-    new DataY(states, 5e3).run();
-    new Deleter(states, 7e3).run();
-    new Viewer(states, 1e3).run();
+function run() {
+    new OrderTicket(states, 300, 10e3).run();
+    new LoadGass(states, 3e3).run();
+    new BuyTicket(states, 5e3).run();
+    new ConfirmTicket(states, 7e3).run();
+    new ViewTicketStates(states, 1e3).run();
 }
