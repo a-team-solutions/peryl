48ef6ee634adce5ba601c6758c78fe1aa55d47e3
Peter Rybar <pr.rybar@gmail.com>
2020-03-24 00:43:17 +0100

    hsml h - types fix

--- src/hsml-h.ts
+++ src/hsml-h.ts
@@ -1,12 +1,12 @@
-import { HElement } from "./hsml";
+import { HElement, HAttrs, HHead, HChildren } from "./hsml";
 
-export function h(tag: string): HElement;
-export function h(tag: string, text: string): HElement;
-export function h(tag: string, attrs: {}): HElement;
-export function h(tag: string, attrs: {}, text: string): HElement;
-export function h(tag: string, attrs: {}, childs: HElement[]): HElement;
-export function h(tag: string, childs: HElement[]): HElement;
-export function h(tag: string, second?: string | {} | HElement[], third?: string | {} | HElement[]): HElement {
+export function h(tag: HHead): HElement;
+export function h(tag: HHead, text: string): HElement;
+export function h(tag: HHead, attrs: HAttrs): HElement;
+export function h(tag: HHead, attrs: HAttrs, text: string): HElement;
+export function h(tag: HHead, attrs: HAttrs, children: string | HChildren): HElement;
+export function h(tag: HHead, children: string | HChildren): HElement;
+export function h(tag: HHead, second?: string | HAttrs | HChildren, third?: string | HChildren): HElement {
     const result: [string, ...any[]] = [tag];
     if (second) {
         if (typeof second === "string") {
