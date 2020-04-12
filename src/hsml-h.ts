3051f9cc7ab5b1efb854974467522206e5decc3b
Filip Seman <seman.filip@gmail.com>
2020-04-12 16:55:51 +0200

    Update hsml-h - Add number possibility as text - Update tests

--- src/hsml-h.ts
+++ src/hsml-h.ts
@@ -1,17 +1,17 @@
 import { HElement, HAttrs, HHead, HFnc, HChildren } from "./hsml";
 
 export function h(tag: HHead): HElement;
-export function h(tag: HHead, text: string): HElement;
+export function h(tag: HHead, text: string | number): HElement;
 export function h(tag: HHead, children: HChildren): HElement;
 export function h(tag: HHead, fnc: HFnc): HElement;
 export function h(tag: HHead, attrs: HAttrs): HElement;
-export function h(tag: HHead, attrs: HAttrs, text: string): HElement;
+export function h(tag: HHead, attrs: HAttrs, text: string | number): HElement;
 export function h(tag: HHead, attrs: HAttrs, children: HElement[]): HElement;
 export function h(tag: HHead, attrs: HAttrs, fnc: HFnc): HElement;
 export function h(tag: HHead, second?: HAttrs | HChildren, third?: HAttrs | HChildren): HElement {
     const result: [string, ...any[]] = [tag];
     if (second) {
-        if (typeof second === "string") {
+        if (typeof second === "string" || typeof second === "number") {
             result.push([second]);
         } else if (Array.isArray(second) || typeof second === "function") {
             result.push(second);
@@ -21,7 +21,7 @@ export function h(tag: HHead, second?: HAttrs | HChildren, third?: HAttrs | HChi
         }
     }
     if (third) {
-        if (typeof third === "string") {
+        if (typeof third === "string" || typeof third === "number") {
             result.push([third]);
         } else if (Array.isArray(third) || typeof third === "function") {
             result.push(third);
